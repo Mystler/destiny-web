32aabf47fc763b2086ef19ec171ef6b71bc4434b
Mystler <161936+Mystler@users.noreply.github.com>
2026-08-21 01:52:16 +0200

    feat: News post

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -10,7 +10,33 @@
 <h1>News</h1>
 
 <div class="p-1">
-  <Card date="2026-08-20">
-    <p>Hi!</p>
+  <Card date="2026-08-21 | Mystler">
+    <p>Hi! Welcome to the new website for Destiny!</p>
+    <p>
+      I have taken some time to create a new website. The old forum was overkill for the simply offering registration
+      services and information to play. Discussion was not really happening much and tends to work better on services
+      like Discord. The forum also came with a lot of fighting AI crawlers that love phpbb giving them so many links to
+      scrape, with and - more often - without consent.
+    </p>
+    <p>
+      Since I recently returned to be more active in URU again, I decided to remake the Destiny website. All of your
+      accounts have been ensured to work seemlessly with the new site. (As long as your account was ever registered with
+      the shard itself. There were some forum accounts that never logged in again to have their actual server account
+      created, so if you happen to be one of them, you will need to register again.)
+    </p>
+    <p>
+      I will likely continue to make some changes to this site but it is now ready in the sense that it offers all the
+      information needed to play, account services to register for the shard, as well as the form to upload fan ages.
+    </p>
+    <p>
+      Furthermore, I have also gone through some things on the Destiny shard. I have properly set a whole lot of global
+      SDLs so that full playthroughs are now actually possible. You should be able to play all the content, including
+      gathering all Relto pages and sparkies.
+    </p>
+    <p>
+      Let me know if you encounter any issues. I am always available on Discord (Tag "mystler") and I also hang out on
+      the OpenURU and Cyan Discord servers.
+    </p>
+    <p>I hope you enjoy the new website and playing on Destiny. ❤️</p>
   </Card>
 </div>
